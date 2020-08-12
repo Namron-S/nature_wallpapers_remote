12125128805e585c3e50de06d2d0e9e32275dcec
Namron-S <61048997+Namron-S@users.noreply.github.com>
2020-08-12 07:20:18 +0200

    init: counter App

---
+++ android/app/src/main/kotlin/com/example/corona_wallpaper_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.corona_wallpaper_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/**
+ * Liste von WallPaperPhotos:
+ * -GridView > GridTile > ImageView(photoModel.src.portrait) > (Hero Widget)
+ * Datei: widget.dart
+ **********************************************************************
+
+ * getTrendingWallpaper:
+ * http.get(
+        "https://api.pexels.com/v1/curated?per_page=$noOfImageToLoad&page=1",
+        headers: {"Authorization": apiKEY}).then((value) {
+      //print(value.body);
+
+      Map<String, dynamic> jsonData = jsonDecode(value.body);
+      jsonData["photos"].forEach((element) {
+        //print(element);
+        PhotosModel photosModel = new PhotosModel();
+        photosModel = PhotosModel.fromMap(element);
+        photos.add(photosModel);
+        //print(photosModel.toString()+ "  "+ photosModel.src.portrait);
+      });
+
+      setState(() {});
+    });
+ * Datei: home.dart
+ */
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
