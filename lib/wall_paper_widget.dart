e597dd35bfb1fa8c7eef83ef13d46962e5ad305b
Namron-S <61048997+Namron-S@users.noreply.github.com>
2020-08-18 15:04:50 +0200

    working with cached NetworkImage instead of Image

--- lib/wall_paper_widget.dart
+++ lib/wall_paper_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 Widget getWallPaperWidget(List<Photo> photoList) {
   return GridView.count(
-    crossAxisCount: 2,
+    crossAxisCount: 3,
     children: photoList
         .map((e) => Center(
-            child: GridTile(child: Image.network('${e.source.portrait}'))))
+            child: GridTile(
+                child: CachedNetworkImage(
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    imageUrl: '${e.source.portrait}'))))
         .toList(),
   );
 }
