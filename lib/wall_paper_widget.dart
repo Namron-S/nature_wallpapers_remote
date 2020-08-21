a510ea4aa8352c0fb4a69d7401ee0cb615681b40
Namron-S <61048997+Namron-S@users.noreply.github.com>
2020-08-21 15:54:11 +0200

    UI: added ActnBttns (stubs) to FullScreen Picture

--- lib/wall_paper_widget.dart
+++ lib/wall_paper_widget.dart
@@ -38,6 +38,24 @@ class DetailScreen extends StatelessWidget {
               placeholder: (context, url) => CircularProgressIndicator(),
               imageUrl: this.photoSource),
         ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            FloatingActionButton(
+              //heroTag muss gesetzt werden, sonst Exception: there are multiple heroes that share the same tag
+              heroTag: 'ButtonHomeScreen',
+              onPressed: null,
+              tooltip: 'Set as Homescreen',
+              child: Icon(Icons.add_to_home_screen),
+            ),
+            FloatingActionButton(
+              heroTag: 'ButtonLockScreen',
+              onPressed: null,
+              tooltip: 'Set as Locksreen',
+              child: Icon(Icons.screen_lock_portrait),
+            ),
+          ],
+        ),
       ),
     );
   }
