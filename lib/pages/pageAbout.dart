05f859920c4193872ef73ad47d6da046890a1590
Namron-S <61048997+Namron-S@users.noreply.github.com>
2020-09-17 13:08:54 +0200

    Implemented AboutPage (dummy versoin)

---
+++ lib/pages/pageAbout.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../drawer.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavigationDrawer(),
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Container(
+        child: Text('Some stuff about pexels.com and the author ;)'),
+      ),
+    );
+  }
+}
