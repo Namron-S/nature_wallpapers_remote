ea53d23f3945fcdaeaa4cc0c19ae9481bf27d70f
Namron-S <61048997+Namron-S@users.noreply.github.com>
2020-10-14 16:01:42 +0200

    DrawerHeader with close option

--- lib/navigation/drawer.dart
+++ lib/navigation/drawer.dart
@@ -4,23 +4,26 @@ import 'navigation.dart' as navi;
 class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          _createHeader(context),
-          _createListTile('Favorites', context),
-          Divider(),
-          _createListTile('Corona', context),
-          _createListTile('Nature', context),
-          _createListTile('City', context),
-          _createListTile('Support', context),
-          _createListTile('Network', context),
-          _createListTile('Teamwork', context),
-          _createListTile('Dark', context),
-          Divider(),
-          _createListTile('About', context),
-        ],
+    return SizedBox(
+      width: 180,
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            _createHeader(context),
+            _createListTile('Favorites', context),
+            Divider(),
+            _createListTile('Corona', context),
+            _createListTile('Nature', context),
+            _createListTile('City', context),
+            _createListTile('Support', context),
+            _createListTile('Network', context),
+            _createListTile('Teamwork', context),
+            _createListTile('Dark', context),
+            Divider(),
+            _createListTile('About', context),
+          ],
+        ),
       ),
     );
   }
@@ -36,10 +39,18 @@ Widget _createListTile(String pageName, BuildContext context) {
 }
 
 Widget _createHeader(BuildContext context) {
-  return DrawerHeader(
-    child: Text('Drawer Header'),
-    decoration: BoxDecoration(
-      color: Theme.of(context).accentColor,
+  return Container(
+    height: 100,
+    child: DrawerHeader(
+      child: ListTile(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        trailing: Icon(Icons.menu),
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+      ),
     ),
   );
 }
